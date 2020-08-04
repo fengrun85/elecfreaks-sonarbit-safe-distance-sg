9b0df332ce625fdb0093dc2541308800111a0776
fengrun85 <fengrun@gmail.com>
2020-08-05 04:10:38 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+OLED.init(128, 64)
+let distance = 0
+basic.forever(function () {
+    distance = Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P16)
+    OLED.writeNumNewLine(distance)
+    led.plotBarGraph(
+    distance,
+    100
+    )
+    if (distance < 100) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    basic.pause(100)
+    OLED.clear()
+})
